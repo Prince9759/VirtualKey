f0418bbb5a4cf8bd077bdbdb0e767a7a3c0947c3
Prince9759 <prince.jain_cs18@gla.ac.in>
2022-07-29 21:05:04 +0530

    Create Main.java
    main class

---
+++ Main.java
@@ -0,0 +1,17 @@
+package virtual;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	public static final String path = "C:\\Users\\pbjai\\eclipse-workspace\\virtual";
+	public static void main(String[] args) {
+
+		menu obj = new menu();
+		obj.screen();
+		obj.mainmenu();
+
+	}
+
+}
